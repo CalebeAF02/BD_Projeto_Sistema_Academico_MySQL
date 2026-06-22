c9806d91cee63838002b223fd4d60bdf635471de
VitorIvan <vitor.ivan2004@gmail.com>
2026-06-22 01:25:04 -0300

    chore: remove pasta duplicada

--- BD_Projeto_Sistema_Academico_MySQL-main/src/python/repositories/base_repository.py
+++
@@ -1,6 +0,0 @@
-class BaseRepository:
-    def __init__(self, connection):
-        self.conn = connection
-
-    def _cursor(self):
-        return self.conn.cursor(dictionary=True)
--- BD_Projeto_Sistema_Academico_MySQL-main/src/sql/04_alter_foto.sql
+++
@@ -1,9 +0,0 @@
--- =====================================================================
--- Adiciona coluna de foto (LONGBLOB) na tabela pessoa.
--- Executar APÓS 00_criar_tabelas.sql, ANTES dos seeds.
--- =====================================================================
-
-USE projeto_unb;
-
-ALTER TABLE pessoa
-    ADD COLUMN foto LONGBLOB;
